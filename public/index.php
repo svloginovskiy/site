c2ebab9ace6bdd07a066ae3c8824912b084fa648
Stepan Loginovskiy <stepan_loginovskiy@dotwrk.com>
2021-08-05 17:44:00 +0700

    Add index.php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+<title>WOW!</title>
+</head>
+<body>
+<?php
+
+    echo "Hello everyone";
+?>
+<img src="https://media.giphy.com/media/sUNqplVFtsctW/giphy.gif" alt="" />
+</body>
+</html>
+
