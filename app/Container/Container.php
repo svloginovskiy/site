ddd84f49796fede26fbd08bf9cde378d9d2610ff
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-08-19 18:44:54 +0700

    Add create function in dependency injection container class 1

--- app/Container/Container.php
+++ app/Container/Container.php
@@ -2,34 +2,59 @@
 
 namespace app\Container;
 
+use Exception;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionNamedType;
+
 class Container
 {
     private $instances = [];
 
     private $rules = [];
 
+    /**
+     * @throws Exception
+     */
     public function create(string $name)
     {
         if (!empty($this->instances[$name])) {
             return $this->instances[$name];
         }
         try {
-            $class = new \ReflectionClass($name);
+            $class = new ReflectionClass($name);
             $constructor = $class->getConstructor();
             $parameters = $constructor->getParameters();
 
-            $constructParamsArrObj = $this->rules[$name]->constructParams;
+            $constructParamsArr = $this->rules[$name]->constructParams;
+            $constructParamsArrObj = new \ArrayObject($this->rules[$name]->constructParams);
             $constructParamsIterator = $constructParamsArrObj->getIterator;
 
+            /*foreach ($parameters as $parameter) {
+                $paramType = $parameter->getType();
+                if (!($paramType instanceof ReflectionNamedType)) {
+                    throw new Exception('Non-typed parameter in constructor');
+                }
+                if (!$paramType->isBuiltin()) {
+                    $this->create($paramType);
+                } else {
+                    if ($constructParamsIterator->valid()) {
+
+                    }
+                }
+            }*/
+
             foreach ($parameters as $parameter) {
-                $paramClass = $parameter->getType();
-                if ($paramClass->isUserDefined()) {
-                    $this->create($paramClass);
+                $paramName = $parameter->getName();
+                if (isset($constructParamsArr[$paramName])) {
+
                 } else {
 
                 }
+
             }
-        } catch (\ReflectionException $exception) {
+
+        } catch (ReflectionException $exception) {
         }
     }
 
