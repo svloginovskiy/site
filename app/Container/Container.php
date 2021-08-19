e98756d69de63a630a380d395623b2c091bf1c07
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-08-19 17:47:41 +0700

    Add create function in dependency injection container class

--- app/Container/Container.php
+++ app/Container/Container.php
@@ -6,8 +6,35 @@ class Container
 {
     private $instances = [];
 
+    private $rules = [];
+
     public function create(string $name)
     {
+        if (!empty($this->instances[$name])) {
+            return $this->instances[$name];
+        }
+        try {
+            $class = new \ReflectionClass($name);
+            $constructor = $class->getConstructor();
+            $parameters = $constructor->getParameters();
 
+            $constructParamsArrObj = $this->rules[$name]->constructParams;
+            $constructParamsIterator = $constructParamsArrObj->getIterator;
+
+            foreach ($parameters as $parameter) {
+                $paramClass = $parameter->getType();
+                if ($paramClass->isUserDefined()) {
+                    $this->create($paramClass);
+                } else {
+
+                }
+            }
+        } catch (\ReflectionException $exception) {
+        }
+    }
+
+    public function addRules(array $rules)
+    {
+        $this->rules = $rules + $this->rules;
     }
 }
