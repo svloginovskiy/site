3d84e436926f6d7924acb801ea40d2709f13448c
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-08-23 20:04:10 +0700

    Add login view

---
+++ resources/login.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Login page</title>
+</head>
+<body>
+<form action="index.php" method="POST">
+    <label>Username: <input type="text" placeholder="Enter username" name="login" required></label>
+    <label>Password: <input type="password" placeholder="Enter password" name="password" required></label>
+
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
