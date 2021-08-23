78ce29c59af0e2926fffcd0c80d04d1b594b371f
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-08-23 14:13:37 +0700

    Add Route class

--- routes/Route.php
+++ routes/Route.php
@@ -8,36 +8,63 @@ class Route
 
     public static function get(string $expression, callable $function)
     {
-        self::routes[$expression] = ["method" => "get", "function" => $function];
+        self::$routes[] = ["expression" => $expression, "method" => "get", "function" => $function];
     }
 
     public static function post(string $expression, callable $function)
     {
-        self::routes[$expression] = ["method" => "post", "function" => $function];
+        self::$routes[] = ["expression" => $expression, "method" => "post", "function" => $function];
     }
 
     public static function put(string $expression, callable $function)
     {
-        self::routes[$expression] = ["method" => "put", "function" => $function];
+        self::$routes[] = ["expression" => $expression, "method" => "put", "function" => $function];
     }
 
     public static function patch(string $expression, callable $function)
     {
-        self::routes[$expression] = ["method" => "patch", "function" => $function];
+        self::$routes[] = ["expression" => $expression, "method" => "patch", "function" => $function];
     }
 
     public static function delete(string $expression, callable $function)
     {
-        self::routes[$expression] = ["method" => "delete", "function" => $function];
+        self::$routes[] = ["expression" => $expression, "method" => "delete", "function" => $function];
     }
 
-    public static function run() {
+    public static function run()
+    {
         $parsedUrl = parse_url($_SERVER['REQUEST_URI']);
         $path = $parsedUrl['path'];
 
+
+
         $method = $_SERVER['REQUEST_METHOD'];
 
+        $pathMatchFound = false;
 
+        $routeMatchFound = false;
+
+        foreach (self::$routes as $route) {
+            $expression = '^' . $route['expression'] . '$';
+
+            if (preg_match('#' . $expression . '#', $path, $matches)) {
+                $pathMatchFound = true;
+
+                if (strtolower($method) == strtolower($route['method'])) {
+                    array_shift($matches);
+                    call_user_func_array($route['function'], $matches);
+                    $routeMatchFound = true;
+                    break;
+                }
+            }
+
+            if (!$routeMatchFound) {
+                if ($pathMatchFound) {
+                    header("HTTP/1.0 405 Method Not Allowed");
+                } else {
+                    header("HTTP/1.0 404 Not Found");
+                }
+            }
+        }
     }
-
 }
