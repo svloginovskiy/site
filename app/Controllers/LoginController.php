b37ff19cffc60dc250bb3bebaaac279f479b24c1
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-08-23 20:03:46 +0700

    Add login controller

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\Controllers;
+
+use View;
+
+class LoginController
+{
+    private $view;
+
+    public function __construct(\app\View\View $view)
+    {
+        $this->view = $view;
+    }
+
+    public function show()
+    {
+        $this->view->render("login.php");
+    }
+
+}
