88d8c85d97374f86ac143eaf4be41706e0d6458b
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-09-01 16:29:36 +0700

    US2-1 Add signup_template.php

---
+++ resources/signup_template.php
@@ -0,0 +1,25 @@
+<div class="container overflow-hidden">
+    <div class="row justify-content-md-center">
+        <div class="col-md-auto border rounded m-3">
+            <form class="form-floating " action="/signup" method="POST">
+                <h1>Sign up</h1>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="usernameInput" placeholder="username" name="name"
+                           required>
+                    <label for="usernameInput">Username</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="emailInput" placeholder="email"
+                           name="email" required>
+                    <label for="emailInput">Email</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="passwordInput" placeholder="password"
+                           name="password" required>
+                    <label for="passwordInput">Password</label>
+                </div>
+                <button type="submit" class="btn btn-primary mb-3">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
