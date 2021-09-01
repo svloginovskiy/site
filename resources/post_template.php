d44acd53505d22c19bd5ca93233602f2c83077f1
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-09-01 16:13:09 +0700

    Change post_template layout using bootstrap

--- resources/post_template.php
+++ resources/post_template.php
@@ -1,3 +1,7 @@
-<div>
-    <p><?= $text; ?></p>
+<div class="container overflow-hidden">
+    <div class="row justify-content-md-center">
+        <div class="col-md-auto border rounded m-3">
+            <p><?= $text; ?></p>
+        </div>
+    </div>
 </div>
