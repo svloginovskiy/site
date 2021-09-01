d3abee7924ce7e7458021515a776dc57f653c6f5
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-09-01 16:05:42 +0700

    Add bootstrap links in html_base_template.php and change css to bootstrap in login_template.php

--- resources/login_template.php
+++ resources/login_template.php
@@ -1,13 +1,20 @@
-<div class="form">
-    <form action="/login" method="POST">
-        <h1>Sign in</h1>
-        <div class="form-label">
-            <label>Username:</label> <input type="text" placeholder="Enter username" name="name" required>
+<div class="container overflow-hidden">
+    <div class="row justify-content-md-center">
+        <div class="col-md-auto border rounded m-3">
+            <form class="form-floating " action="/login" method="POST">
+                <h1>Log in</h1>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="usernameInput" placeholder="username" name="name"
+                           required>
+                    <label for="usernameInput">Username</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="passwordInput" placeholder="password"
+                           name="password" required>
+                    <label for="passwordInput">Password</label>
+                </div>
+                <button type="submit" class="btn btn-primary mb-3">Log in</button>
+            </form>
         </div>
-        <div class="form-label">
-            <label>Password:</label> <input type="password" placeholder="Enter password"
-                                            name="password" required>
-        </div>
-        <button type="submit">Login</button>
-    </form>
+    </div>
 </div>
