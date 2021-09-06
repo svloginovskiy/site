4030c6693a4c7dc03219f3b1328416e18de177f0
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-09-06 15:07:20 +0700

    US2-3 Change font to Roboto for h1 tag in posts

--- resources/html_base_template.php
+++ resources/html_base_template.php
@@ -6,12 +6,15 @@
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="/css/<?= file_exists('css/' . $VIEWNAME . '.css') ? $VIEWNAME . '.css' : 'default.css'; ?>"
           rel="stylesheet"/>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+    </style>
 </head>
 <body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+
 <?php
 include $VIEWBODY ?>
 </body>
