a79175946757ddb5b681030f0fadca38abd8ef67
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-09-06 14:09:24 +0700

    Change grammar in signup_welcome_template.php

--- resources/signup_welcome_template.php
+++ resources/signup_welcome_template.php
@@ -1,4 +1,4 @@
 <div class="container d-flex text-center w-100 h-100 p-3 mx-auto flex-column">
     <h1>Welcome, <?= $name ?>!</h1>
-    <p>You are successfully signed up, now you can <a href="/login">log in</a>.</p>
+    <p>You have successfully signed up, now you can <a href="/login">log in</a>.</p>
 </div>
