d0dbebd32949fa5cdbe01de6c060103310453ab1
svloginovskiy <stepan_loginovskiy@dotwrk.com>
2021-09-14 18:17:35 +0700

    Add default.css to all pages

--- resources/html_base_template.php
+++ resources/html_base_template.php
@@ -4,10 +4,13 @@
     <title><?= isset($VIEWTITLE) ? ucfirst($VIEWTITLE) : ucfirst($VIEWNAME) . ' page'; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="/css/<?= file_exists('css/' . $VIEWNAME . '.css') ? $VIEWNAME . '.css' : 'default.css'; ?>"
-          rel="stylesheet"/>
+    <?php if (file_exists('css/' . $VIEWNAME . '.css')): ?>
+        <link href="/css/<?= $VIEWNAME . '.css'; ?>"
+              rel="stylesheet"/>
+    <?php endif; ?>
+    <link href="/css/default.css" rel="stylesheet"/>
     <?php if (file_exists('js/' . $VIEWNAME . '.js')): ?>
-    <script src="/js/<?= $VIEWNAME; ?>.js"></script>
+        <script src="/js/<?= $VIEWNAME; ?>.js"></script>
     <?php endif; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
