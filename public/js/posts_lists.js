188be1c8c568e35f6312adbdbaacff6ba8a3f150
svloginovskiy <svloginovskiy@gmail.com>
2021-09-20 21:28:23 +0700

    US4-2 Add Post rating on frontpage

--- public/js/posts_lists.js
+++ public/js/posts_lists.js
@@ -1,10 +1,13 @@
 function initElement() {
     let postCards = document.querySelectorAll('.postCard');
-    postCards.forEach(function (postCard,) {
-        postCard.addEventListener('click', function () {
-            let link = (postCard.getElementsByTagName('a'))[0];
+    let articles = document.getElementsByTagName('article');
+    Array.from(articles).forEach(function (article) {
+        article.addEventListener('click', function () {
+            let link = (article.getElementsByTagName('a'))[0];
             link.click();
         });
+    });
+    postCards.forEach(function (postCard) {
         postCard.addEventListener('mouseover', function () {
             postCard.classList.toggle('shadow');
             postCard.classList.toggle('shadow-lg');
@@ -12,6 +15,78 @@ function initElement() {
         postCard.addEventListener('mouseout', function () {
             postCard.classList.toggle('shadow');
             postCard.classList.toggle('shadow-lg');
-        })
+        });
+
+        let rateDiv = postCard.getElementsByClassName('rateDiv')[0];
+        let rateUpDiv = postCard.getElementsByClassName('rateUpDiv')[0];
+        let rateDownDiv = postCard.getElementsByClassName('rateDownDiv')[0];
+        let ratingDiv = postCard.getElementsByClassName('rating')[0];
+        if (rateUpDiv != null && rateDownDiv != null) {
+            let rateUpIcon = rateUpDiv.firstElementChild;
+            let rateDownIcon = rateDownDiv.firstElementChild;
+
+            let rateUpDivClicked = rateUpIcon.classList.contains('bi-arrow-up-square-fill');
+            let rateDownDivClicked = rateDownIcon.classList.contains('bi-arrow-down-square-fill');
+
+            rateUpDiv.addEventListener('click', function (e) {
+                rateUpDivClicked = !rateUpDivClicked;
+                rateDownDivClicked = rateUpDivClicked ? false : rateDownDivClicked;
+                let upXhr = new XMLHttpRequest();
+                let curPath = (new URL(document.URL)).pathname;
+                upXhr.open('POST', '/posts/' + postCard.id + '/upvote', true);
+                upXhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                upXhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                upXhr.onreadystatechange = function () {
+                    if (upXhr.readyState == 4 && upXhr.status == 200) {
+                        ratingDiv.textContent = upXhr.responseText;
+                    }
+                };
+                upXhr.send();
+            });
+            rateDownDiv.addEventListener('click', function (e) {
+                rateDownDivClicked = !rateDownDivClicked;
+                rateUpDivClicked = rateDownDivClicked ? false : rateUpDivClicked;
+                let curPath = (new URL(document.URL)).pathname;
+                let downXhr = new XMLHttpRequest();
+                downXhr.open('POST', '/posts/' + postCard.id + '/downvote', true);
+                downXhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                downXhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                downXhr.onreadystatechange = function () {
+                    if (downXhr.readyState == 4 && downXhr.status == 200) {
+                        ratingDiv.textContent = downXhr.responseText;
+                    }
+                };
+                downXhr.send();
+            });
+            rateDiv.addEventListener('click', function (e) {
+
+                if (rateUpDivClicked) {
+                    rateUpIcon.classList.remove('bi-arrow-up-square');
+                    rateUpIcon.classList.add('bi-arrow-up-square-fill');
+                } else {
+                    rateUpIcon.classList.remove('bi-arrow-up-square-fill');
+                    rateUpIcon.classList.add('bi-arrow-up-square');
+                }
+                if (rateDownDivClicked) {
+                    rateDownIcon.classList.remove('bi-arrow-down-square');
+                    rateDownIcon.classList.add('bi-arrow-down-square-fill');
+                } else {
+                    rateDownIcon.classList.remove('bi-arrow-down-square-fill');
+                    rateDownIcon.classList.add('bi-arrow-down-square');
+                }
+            });
+        }
+
+        let rateUpDivDisabled = postCard.getElementsByClassName('rateUpDivDisabled')[0];
+        let rateDownDivDisabled = postCard.getElementsByClassName('rateDownDivDisabled')[0];
+        if (rateUpDivDisabled != null && rateDownDivDisabled != null) {
+            rateUpDivDisabled.addEventListener('click', function (e) {
+                location.href = "/login";
+            });
+            rateDownDivDisabled.addEventListener('click', function (e) {
+                location.href = "/login";
+            });
+        }
     });
+
 }
--- resources/posts_list_template.php
+++ resources/posts_list_template.php
@@ -1,22 +1,22 @@
 <script src="/js/posts_lists.js"></script>
-<div onload="initElement()" hidden></div>
+<div onload="initElement();" hidden></div>
 <?php foreach ($posts as $post) : ?>
     <div class="row justify-content-md-center">
-        <div class="border rounded m-3 shadow postCard" style="width: 40rem; cursor: pointer;">
-            <article>
+        <div class="border rounded m-3 shadow postCard" id="<?=$post['id']; ?>" style="width: 40rem;">
+            <article style="cursor: pointer;">
                 <a href="/posts/<?= $post['id']; ?>"></a>
                 <h1><?= $post['title']; ?></h1>
                 <?= $post['text']; ?>
             </article>
             <div class="d-flex mb-2">
-                <div class="ms-auto d-flex" id="rateDiv">
-                    <div class id="<?= $isLoggedIn  ? 'rateUpDiv' : 'rateUpDivDisabled'; ?>"  >
+                <div class="ms-auto d-flex rateDiv">
+                    <div class="<?= $isLoggedIn  ? 'rateUpDiv' : 'rateUpDivDisabled'; ?>"  >
                         <i class="bi <?= $post['isUpvoted'] ? 'bi-arrow-up-square-fill' : 'bi-arrow-up-square'; ?>"></i>
                     </div>
-                    <div id="rating">
+                    <div class="rating">
                         <?= $post['rating']; ?>
                     </div>
-                    <div id="<?= $isLoggedIn  ? 'rateDownDiv' : 'rateDownDivDisabled'; ?>">
+                    <div class="<?= $isLoggedIn  ? 'rateDownDiv' : 'rateDownDivDisabled'; ?>">
                         <i class="bi <?= isset($post['isUpvoted']) ? ($post['isUpvoted'] ? 'bi-arrow-down-square' : 'bi-arrow-down-square-fill') : 'bi-arrow-down-square'; ?>"></i>
                     </div>
                 </div>
