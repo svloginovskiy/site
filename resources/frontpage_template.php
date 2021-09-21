69e1c9c92a590d3cd510c924159903d02dadf8e3
svloginovskiy <svloginovskiy@gmail.com>
2021-09-21 18:26:09 +0700

    US4-3 Change select position and size

--- resources/frontpage_template.php
+++ resources/frontpage_template.php
@@ -1,13 +1,15 @@
 <div class="container overflow-hidden">
-    <div class="row justify-content-md-center mt-1">
-        <div class="col-3">
-            <p class="text-end">Sorted by</p>
-        </div>
-        <div class="col-3">
-            <select onchange="handleSelect();" class="form-select">
-                <option <?= $sortedBy == 'time' ? 'selected' : ''; ?> value="time">Time</option>
-                <option <?= $sortedBy == 'rating' ? 'selected' : ''; ?> value="rating">Rating</option>
-            </select>
+    <div class="row mt-1 justify-content-md-center">
+        <div class="d-flex" style="width: 40rem;">
+            <div class="ms-auto">
+                <div class="align-middle d-inline-block">Sorted by</div>
+                <div class="d-inline-block">
+                    <select onchange="handleSelect();" class=" form-select" style="width: ">
+                        <option <?= $sortedBy == 'time' ? 'selected' : ''; ?> value="time">Time</option>
+                        <option <?= $sortedBy == 'rating' ? 'selected' : ''; ?> value="rating">Rating</option>
+                    </select>
+                </div>
+            </div>
         </div>
     </div>
     <?php
@@ -18,12 +20,14 @@
                 <a class="page-link" href="/<?= $sortedBy == 'rating' ? 'top/' : ''; ?>">&laquo;</a>
             </li>
             <li class="page-item <?= $prevActive ? 'active' : ''; ?>"><a class="page-link"
-                                                                         href="/<?= $sortedBy == 'rating' ? 'top/' : ''; ?><?= $prev; ?>"><?= $prev; ?></a></li>
+                                                                         href="/<?= $sortedBy == 'rating' ? 'top/' : ''; ?><?= $prev; ?>"><?= $prev; ?></a>
+            </li>
             <li class="page-item <?= $curActive ? 'active' : ''; ?>"><a class="page-link"
                                                                         href="/<?= $sortedBy == 'rating' ? 'top/' : ''; ?><?= $current; ?>"><?= $current; ?></a>
             </li>
             <li class="page-item <?= $nextActive ? 'active' : ''; ?>"><a class="page-link"
-                                                                         href="/<?= $sortedBy == 'rating' ? 'top/' : ''; ?><?= $next; ?>"><?= $next; ?></a></li>
+                                                                         href="/<?= $sortedBy == 'rating' ? 'top/' : ''; ?><?= $next; ?>"><?= $next; ?></a>
+            </li>
             <li class="page-item">
                 <a class="page-link" href="/<?= $sortedBy == 'rating' ? 'top/' : ''; ?><?= $last; ?>">&raquo;</a>
             </li>
